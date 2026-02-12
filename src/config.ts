649554d7928e38fe3380679f391786219af9c610
ibrasyad <91681404+ibrasyad@users.noreply.github.com>
2026-02-12 15:14:23 +0700

    change logo and favicon

--- src/config.ts
+++ src/config.ts
@@ -52,13 +52,13 @@ export const siteConfig: SiteConfig = {
 	// 顶栏标题配置
 	navbarTitle: {
 		// 显示模式："text-icon" 显示图标+文本，"logo" 仅显示Logo
-		mode: "text-icon",
+		mode: "logo",
 		// 顶栏标题文本
 		text: "Ibnulab",
 		// 顶栏标题图标路径，默认使用 public/assets/home/home.png
 		icon: "assets/home/avatar.webp",
 		// 网站Logo图片路径
-		logo: "assets/home/default-logo.png",
+		logo: "assets/home/ibnulab logo.webp",
 	},
 
 	// 页面自动缩放配置
